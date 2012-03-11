33f6083ed3ba18140ae9ea3d7bd498f6c97e5896
Kit Plummer <kitplummer@gmail.com>
2012-03-10 18:38:04 -0700

    add s

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,8 @@
 class artifactory($jdk = "java-1.6.0-openjdk",
-  $sourceforge_mirror = "http://download.sourceforge.net/project",
+  $sourceforge_mirror = "http://downloads.sourceforge.net/project",
   $version = "2.5.1.1") {
 
-#  http://downloads.sourceforge.net/project/artifactory/artifactory/2.5.1.1/artifactory-2.5.1.1.rpm
+# http://downloads.sourceforge.net/project/artifactory/artifactory/2.5.1.1/artifactory-2.5.1.1.rpm?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fartifactory%2Ffiles%2Fartifactory%2F2.5.1.1%2Fartifactory-2.5.1.1.rpm%2Fdownload&ts=1331429681&use_mirror=voxel
 
   # wget from https://github.com/maestrodev/puppet-wget
   include wget
