63b521c052fd4cce0dd6738304c53da2c3cdc529
existsec <existsec@gmail.com>
2015-11-16 12:09:19 -0800

    updated url and version, cleaned double quotes and interpolated variables

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,26 +1,27 @@
-class artifactory($jdk = "java-1.7.0-openjdk",
-  $sourceforge = "http://downloads.sourceforge.net/project",
-  $version = "3.0.1") {
+class artifactory(
+  $jdk = 'java-1.8.0-openjdk',
+  $bintray = 'https://bintray.com/artifact/download/jfrog/artifactory/',
+  $version = '4.2.2') {
 
-# http://downloads.sourceforge.net/project/artifactory/artifactory/2.5.1.1/artifactory-2.5.1.1.rpm
+  # https://bintray.com/artifact/download/jfrog/artifactory/jfrog-artifactory-oss-4.2.2.zip
 
   if ! defined (Package[$jdk]) {
     package { $jdk: ensure => installed }
   }
 
   package { 'artifactory':
-    ensure => installed,
-    provider => "rpm",
-    source => "$sourceforge/artifactory/artifactory/$version/artifactory-$version.rpm",
-    require => Package[$jdk]
+    ensure   => installed,
+    provider => 'rpm',
+    source   => "${bintray}/jfrog-artifactory-oss-${version}.zip",
+    require  => Package[$jdk]
   }
 
   service { 'artifactory':
     ensure    => 'running',
-    enable => "true",
-    hasstatus => "false",
-    provider => "redhat",
-    require => Package['artifactory'] 
+    enable    => true,
+    hasstatus => false,
+    provider  => 'redhat',
+    require   => Package['artifactory']
   }
 
 }
